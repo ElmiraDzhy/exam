b9d5a96c37b060be5d01f6ee3dc08e9215930820
Elmira Dzhy <elmira.freshcode@gmail.com>
2023-08-10 16:02:26 +0300

    [Refactor] Update toast notifications for better user experience
    [Issue #5]

--- client/src/components/GeneralCounter/index.jsx
+++ client/src/components/GeneralCounter/index.jsx
@@ -1,59 +1,49 @@
 import React, {useEffect} from "react";
 import {toast, ToastContainer} from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
-
+import "./toastifyCustomStyle.scss";
 const GeneralCounter = (props) => {
 
     const notify = (eventName) => {
-        toast.success(`${eventName} time is over`, {
+        toast.success(`Upcoming event: ${eventName} will start soon.`, {
             position: 'top-right',
-            autoClose: 2000,
+            autoClose: 5000,
             hideProgressBar: false,
             closeOnClick: true,
             pauseOnHover: false,
             draggable: true,
+            className: "custom-toast",
+            bodyClassName: "custom-toast-body",
+            progressClassName: "custom-toast-progress",
         });
     };
 
     useEffect(() => {
-        if(JSON.parse(localStorage.getItem('events'))){
-            console.log('ALREADY EXIST')
-        }else{
-            console.log('NOT EXIST - MAKING')
-            localStorage.setItem('events', JSON.stringify({}))
+        if(!JSON.parse(localStorage.getItem('events'))){
+            localStorage.setItem('events', JSON.stringify({}));
         }
 
-        if(JSON.parse(localStorage.getItem('eventNamesArray'))){
-            console.log('ARRAY OF NAME ALREADY EXIST')
-        }else{
+        if(!JSON.parse(localStorage.getItem('eventNamesArray'))){
             localStorage.setItem('eventNamesArray', JSON.stringify([]));
         }
 
         const intervalId = setInterval(() => {
-
             const localStoreEvents = JSON.parse(localStorage.getItem('events'));
-            const eventNames = Object.keys(localStoreEvents);
 
-            let arrayForReminderEvents = JSON.parse(localStorage.getItem('eventNamesArray'));
+            if(Object.keys(localStoreEvents).length !== 0){
+                const eventNames = Object.keys(localStoreEvents);
+                let arrayForReminderEvents = JSON.parse(localStorage.getItem('eventNamesArray'));
 
-            eventNames.forEach(event => {
-                if (new Date(localStoreEvents[event].reminderDate) < new Date()) {
-                    if(!arrayForReminderEvents.includes(event)){
-                        notify(event);
-                        arrayForReminderEvents.push(event);
-                        localStorage.setItem('eventNamesArray', JSON.stringify(arrayForReminderEvents));
+                eventNames.forEach(event => {
+                    if (new Date(localStoreEvents[event].reminderDate) < new Date()) {
+                        if(!arrayForReminderEvents.includes(event)){
+                            notify(event);
+                            arrayForReminderEvents.push(event);
+                            localStorage.setItem('eventNamesArray', JSON.stringify(arrayForReminderEvents));
+                        }
                     }
-                }
-            });
-
-            eventNames.forEach(event => {
-                if(new Date(localStoreEvents[event].deadlineDate) < new Date()){
-                    arrayForReminderEvents = arrayForReminderEvents.filter(item => item!==event);
-                    delete localStoreEvents[event];
-                    localStorage.setItem('events', JSON.stringify(localStoreEvents));
-                    localStorage.setItem('eventNamesArray', JSON.stringify(arrayForReminderEvents));
-                }
-            })
+                });
+            }
         }, 5000);
 
         return () => {
