f9d5e91cffc5875869b136f91a31ffa53ef2aebe
Elmira Dzhy <elmira.freshcode@gmail.com>
2023-08-11 12:40:12 +0300

    [Feature] Create ButtonElement component with styles
    [Issue #6]

---
+++ client/src/components/ButtonGroup/ButtonElement/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import classNames from "classnames";
+import styles from './ButtonElement.module.scss';
+
+const ButtonElement = (props) => {
+ const {content, checked, changeMode} = props;
+
+    const cnBtn = classNames(styles['button-container'], {
+        [styles['button-checked']]: checked
+    });
+
+    const cnFlag = classNames(styles['flag-default'], {
+        [styles['flag-checked']]: checked
+    });
+
+    return <button onClick={() => changeMode(content.id)} className={cnBtn}>
+                <span className={cnFlag}>{content.flag}</span>
+                <p>{content.text}</p>
+           </button>
+}
+
+export default ButtonElement;
