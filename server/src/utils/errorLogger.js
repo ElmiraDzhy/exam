f1f946087408ab387bbcd6d28e9d600e056f1999
Elmira Dzhy <elmira.freshcode@gmail.com>
2023-08-13 16:06:17 +0300

    [Feature] Add errorLogger.js
    [Issue #12]

---
+++ server/src/utils/errorLogger.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const path = require('path');
+
+function logError(error) {
+  const logDirectory = path.join(__dirname, '..', 'logs');
+
+  if (!fs.existsSync(logDirectory)) {
+    fs.mkdirSync(logDirectory);
+  }
+
+  const logFilePath = path.join(logDirectory, 'buffer');
+
+  const logEntry = {
+    message: error.message || '',
+    time: Date.now(),
+    code: error.code || 0,
+    stackTrace: error.stack || {},
+  };
+
+  const logString = JSON.stringify(logEntry, null, 2) + '\n';
+
+  fs.appendFile(logFilePath, logString, (err) => {
+    if (err) {
+      console.error('Error writing to log file:', err);
+    }
+  });
+}
+
+module.exports = logError;
