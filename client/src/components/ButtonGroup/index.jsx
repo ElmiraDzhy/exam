0e74abe708fa999ea00d35c86ddd09c9bfaa3fee
Elmira Dzhy <elmira.freshcode@gmail.com>
2023-08-16 00:09:37 +0300

    [Refactor] Refactor ButtonGroup index file

--- client/src/components/ButtonGroup/index.jsx
+++ client/src/components/ButtonGroup/index.jsx
@@ -1,30 +1,42 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import ButtonElement from './ButtonElement';
 import styles from './ButtonGroup.module.scss';
 
 const buttonsContent = [{
-    id: 1,
-    flag: 'yes',
-    text: 'The Domain should exactly match the name',
+  id: 1,
+  flag: 'yes',
+  text: 'The Domain should exactly match the name',
 }, {
-    id: 2,
-    flag: 'yes',
-    text: 'But minor variations are allowed (Recommended)',
+  id: 2,
+  flag: 'yes',
+  text: 'But minor variations are allowed (Recommended)',
 }, {
-    id: 3,
-    flag: 'no',
-    text: 'I am only looking for a name, not a Domain',
+  id: 3,
+  flag: 'no',
+  text: 'I am only looking for a name, not a Domain',
 }];
 
-const ButtonGroup = (props) => {
+const ButtonGroup = () => {
+  const [checkedButtonId, setCheckedButton] = useState(2);
 
-    const [checkedButtonId, setCheckedButton] = useState(2);
-
-    return(
-        <article className={styles['button-group-container']}>
-            {buttonsContent.map(item => <ButtonElement content={item} checked={checkedButtonId === item.id} changeMode={setCheckedButton} key={item.id}/>)}
-        </article>
-    );
-}
+  return (
+    <article className={styles['button-group-container']}>
+      {
+          buttonsContent.map(
+            (item) => {
+              return (
+                <ButtonElement
+                  content={item}
+                  checked={checkedButtonId === item.id}
+                  changeMode={setCheckedButton}
+                  key={item.id}
+                />
+              );
+            },
+          )
+      }
+    </article>
+  );
+};
 
 export default ButtonGroup;
