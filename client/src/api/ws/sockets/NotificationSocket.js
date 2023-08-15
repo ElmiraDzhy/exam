8d7eea5e5c027d151101992cca17a6acda6ce44d
Elmira Dzhy <elmira.freshcode@gmail.com>
2023-08-16 00:03:13 +0300

    [Refactor] Refactor NotificationSocket.js - remove useless constructor and fix name of the unsubscribe method

--- client/src/api/ws/sockets/NotificationSocket.js
+++ client/src/api/ws/sockets/NotificationSocket.js
@@ -4,10 +4,6 @@ import WebSocket from './WebSocket';
 import Notification from '../../../components/Notification/Notification';
 
 class NotificationSocket extends WebSocket {
-  constructor(dispatch, getState, room) {
-    super(dispatch, getState, room);
-  }
-
     anotherSubscribes = () => {
       this.onEntryCreated();
       this.onChangeMark();
@@ -36,7 +32,7 @@ class NotificationSocket extends WebSocket {
       this.socket.emit('subscribe', id);
     };
 
-    unsubsctibe = (id) => {
+    unsubscribe = (id) => {
       this.socket.emit('unsubscribe', id);
     }
 }
