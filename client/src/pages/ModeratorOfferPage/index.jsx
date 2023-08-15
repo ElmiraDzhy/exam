8140272765ca97ddb4e5d38e1c8d412f69d9d346
Elmira Dzhy <elmira.freshcode@gmail.com>
2023-08-16 02:04:43 +0300

    [Refactor] Refactor ModeratorOfferPage - apply eslint and add propTypes

--- client/src/pages/ModeratorOfferPage/index.jsx
+++ client/src/pages/ModeratorOfferPage/index.jsx
@@ -1,76 +1,117 @@
-import React, {useEffect} from 'react';
-import {connect} from 'react-redux';
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import Footer from '../../components/Footer/Footer';
-import {getOffersRequest, confirmOfferRequest, rescindOfferRequest} from '../../actions/actionCreator';
+import { getOffersRequest, confirmOfferRequest, rescindOfferRequest } from '../../actions/actionCreator';
 import OfferForModerator from './OfferForModerator';
 import styles from './ModeratorOfferPage.module.scss';
 
-import {useHistory} from "react-router-dom";
 const ModeratorOfferPage = (props) => {
+  const {
+    userStore: { data },
+    offersReducer: { isFetching, error, offers },
+  } = props;
+  const history = useHistory();
 
-    const {userStore: {data},
-        offersReducer: {isFetching, error, offers},
-        getOffersRequest,
-        confirmOfferRequest,
-        rescindOfferRequest} = props;
-    const history = useHistory();
-
-    useEffect(() => {
-        if(!data || data.role !== 'moderator'){
-            history.replace('/login');
-        }
-        getOffersRequest({ limit: 3});
-
-    },[]);
-
-    const confirmOfferHandler = (data) => {
-        confirmOfferRequest(data);
+  useEffect(() => {
+    if (!data || data.role !== 'moderator') {
+      history.replace('/login');
     }
+    getOffersRequest({ limit: 3 });
+  }, []);
 
-    const rescindOfferHandler = (data) => {
-        rescindOfferRequest(data)
-    }
+  const confirmOfferHandler = (payload) => {
+    confirmOfferRequest(payload);
+  };
 
-    const  loadMore = (startFrom) => {
-        getOffersRequest({
-            limit: 3,
-            offset: startFrom,
-        });
-        scrollToBottom();
-    };
+  const rescindOfferHandler = (payload) => {
+    rescindOfferRequest(payload);
+  };
 
-    const scrollToBottom = () => {
-        window.scrollTo({
-            top: document.body.scrollHeight,
-            behavior: 'smooth',
-        });
-    };
+  const scrollToBottom = () => {
+    window.scrollTo({
+      top: document.body.scrollHeight,
+      behavior: 'smooth',
+    });
+  };
 
-    return(
-        <>
-            <section className={styles['header-container']}>
-                <div className={styles['fixed-header']}>
-                    <span className={styles['info']}>Squadhelp recognized as one of the Most Innovative Companies by Inc Magazine.</span>
-                    <a href="#">Read Announcement</a>
-                </div>
-            </section>
-            {
-                 data && data.role !== 'moderator' ? <p>Only for moderator page</p> : <section className={styles.container}>
-                    { isFetching || error || <>
-                        <section>{offers.map(offer => <OfferForModerator key={offer.id} offer={offer} confirm={confirmOfferHandler} rescind={rescindOfferHandler}/>)}</section>
-                    </> }
-                    <button className={styles['load-more-button']} onClick={() => loadMore(offers.length)}>Load More <img src={'/staticImages/load-more.svg'} alt={'load more button'}/></button>
-                </section>
+  const loadMore = (startFrom) => {
+    getOffersRequest({
+      limit: 3,
+      offset: startFrom,
+    });
+    scrollToBottom();
+  };
+
+  return (
+    <>
+      <section className={styles['header-container']}>
+        <div className={styles['fixed-header']}>
+          <span
+            className={styles.info}
+          >
+            Squadhelp recognized as one of the Most Innovative Companies by Inc Magazine.
+          </span>
+          <button type="button">Read Announcement</button>
+        </div>
+      </section>
+      {
+                 data && data.role !== 'moderator' ? <p>Only for moderator page</p> : (
+                   <section className={styles.container}>
+                     { isFetching || error || (
+                     <>
+                       <section>
+                         {
+                               offers.map((offer) => {
+                                 return (
+                                   <OfferForModerator
+                                     key={offer.id}
+                                     offer={offer}
+                                     confirm={confirmOfferHandler}
+                                     rescind={rescindOfferHandler}
+                                   />
+                                 );
+                               })
+                           }
+                       </section>
+                     </>
+                     ) }
+                     <button
+                       className={styles['load-more-button']}
+                       onClick={() => loadMore(offers.length)}
+                       type="button"
+                     >
+                       Load More
+                       <img src="/staticImages/load-more.svg" alt="load more button" />
+                     </button>
+                   </section>
+                 )
             }
-        <Footer/>
-        </>
-    )
-}
+      <Footer />
+    </>
+  );
+};
 
-const mapStateToProps = ({userStore, offersReducer}) => ({userStore, offersReducer});
+const mapStateToProps = ({ userStore, offersReducer }) => ({ userStore, offersReducer });
 const mapDispatchToProps = {
-    getOffersRequest,
-    confirmOfferRequest,
-    rescindOfferRequest
-}
+  getOffersRequest,
+  confirmOfferRequest,
+  rescindOfferRequest,
+};
+
+ModeratorOfferPage.propTypes = {
+  userStore: PropTypes.shape({
+    data: PropTypes.shape({
+      role: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+
+  offersReducer: PropTypes.shape({
+    isFetching: PropTypes.bool,
+    error: PropTypes.shape({}),
+    offers: PropTypes.arrayOf(PropTypes.object),
+  }).isRequired,
+};
+
 export default connect(mapStateToProps, mapDispatchToProps)(ModeratorOfferPage);
