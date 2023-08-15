ac342452e82a4f10da07d3fba0fddfe0bda3a7f8
Elmira Dzhy <elmira.freshcode@gmail.com>
2023-08-16 02:31:35 +0300

    [Refactor] Refactor DragNDropArea - apply eslint and propTypes

--- client/src/components/DragNDropArea/index.jsx
+++ client/src/components/DragNDropArea/index.jsx
@@ -1,78 +1,91 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import React, { useState } from 'react';
-import styles from './DragNDropArea.module.scss';
 import classNames from 'classnames';
+import PropTypes from 'prop-types';
 import { useField } from 'formik';
+import styles from './DragNDropArea.module.scss';
 
 function DragNDropArea(props) {
-    const [field, , helpers] = useField(props.name);
-    const [dragOn, setDragOn] = useState(false);
-    const [previewSrc, setPreviewSrc] = useState(null);
+  const { name } = props;
 
-    const className = classNames(styles.container, {
-        [styles.active]: dragOn,
-    });
+  const [field, , helpers] = useField(name);
+  const [dragOn, setDragOn] = useState(false);
+  const [previewSrc, setPreviewSrc] = useState(null);
 
-    const dragLeave = () => {
-        setDragOn(false);
+  const className = classNames(styles.container, {
+    [styles.active]: dragOn,
+  });
+
+  const dragLeave = () => {
+    setDragOn(false);
+  };
+
+  const dragOver = () => {
+    setDragOn(true);
+  };
+
+  const inputChangeHandler = (e) => {
+    setDragOn(false);
+    const reader = new FileReader();
+    helpers.setValue(e.target.files[0]);
+    reader.onload = (event) => {
+      setPreviewSrc(event.target.result);
     };
+    reader.readAsDataURL(e.target.files[0]);
+  };
 
-    const dragOver = () => {
-        setDragOn(true);
-    };
+  const clearInput = () => {
+    setPreviewSrc(''); // Clear the local state
+    helpers.setValue(''); // Clear the Formik state
+  };
 
-    const inputChangeHandler = (e) => {
-        setDragOn(false);
-        const reader = new FileReader();
-        helpers.setValue(e.target.files[0]);
-        reader.onload =  (event) => {
-            setPreviewSrc(event.target.result);
-        };
-        reader.readAsDataURL(e.target.files[0]);
+  const imgPreviewLayout = (
+    <>
+      <img
+        src={previewSrc}
+        alt="preview"
+        className={styles['img-preview']}
+      />
+      <div className={styles.wrapper}>
+        <button
+          onClick={clearInput}
+          className={styles.btn}
+          type="button"
+        >
+          Choose another
+        </button>
+      </div>
+    </>
+  );
 
-    };
+  const inputLayout = (
+    <>
+      <input
+        {...field}
+        {...props}
+        value={undefined}
+        type="file"
+        className={styles['drag-input']}
+        onChange={inputChangeHandler}
+        onDragLeave={dragLeave}
+        onDragOver={dragOver}
+      />
+      <i className="fas fa-upload" />
+      <h3>Drag&Drop files here</h3>
+      or
+      <button type="button" className={styles['label-btn']}>Browse files</button>
+    </>
+  );
 
-    const clearInput = () => {
-        setPreviewSrc(""); // Clear the local state
-        helpers.setValue(""); // Clear the Formik state
-    };
-
-    const imgPreviewLayout = (
-        <>
-            <img
-                src={previewSrc}
-                alt="preview"
-                className={styles["img-preview"]}
-            />
-            <div className={styles.wrapper}>
-                <button
-                    onClick={clearInput}
-                    className={styles["btn"]}>
-                    Choose another
-                </button>
-            </div>
-        </>
-    );
-
-    const inputLayout = (
-        <>
-            <input
-                {...field}
-                {...props}
-                value={undefined}
-                type="file"
-                className={styles["drag-input"]}
-                onChange={inputChangeHandler}
-                onDragLeave={dragLeave}
-                onDragOver={dragOver}
-            />
-            <i class="fas fa-upload"></i>
-            <h3>Drag&Drop files here</h3>
-            or
-            <label className={styles["label-btn"]}>Browse files</label>
-        </>
-    );
-
-    return <div className={className}>{(previewSrc && field.value) ? imgPreviewLayout : inputLayout}</div>;
+  return (
+    <div className={className}>
+      {(previewSrc && field.value) ? imgPreviewLayout : inputLayout}
+    </div>
+  );
 }
+
+DragNDropArea.propTypes = {
+  name: PropTypes.string.isRequired,
+};
 
 export default DragNDropArea;
