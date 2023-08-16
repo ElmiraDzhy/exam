644b12d79d8668cbb3601d617dddf9615c16a6bb
Elmira Dzhy <elmira.freshcode@gmail.com>
2023-08-16 11:28:21 +0300

    [Refactor] Refactor ButtonElement - apply eslint and propTypes

--- client/src/components/ButtonGroup/ButtonElement/index.jsx
+++ client/src/components/ButtonGroup/ButtonElement/index.jsx
@@ -1,22 +1,39 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import styles from './ButtonElement.module.scss';
 
 const ButtonElement = (props) => {
- const {content, checked, changeMode} = props;
+  const { content, checked, changeMode } = props;
 
-    const cnBtn = classNames(styles['button-container'], {
-        [styles['button-checked']]: checked
-    });
+  const cnBtn = classNames(styles['button-container'], {
+    [styles['button-checked']]: checked,
+  });
 
-    const cnFlag = classNames(styles['flag-default'], {
-        [styles['flag-checked']]: checked
-    });
+  const cnFlag = classNames(styles['flag-default'], {
+    [styles['flag-checked']]: checked,
+  });
 
-    return <button onClick={() => changeMode(content.id)} className={cnBtn}>
-                <span className={cnFlag}>{content.flag}</span>
-                <p>{content.text}</p>
-           </button>
-}
+  return (
+    <button onClick={() => changeMode(content.id)} className={cnBtn} type="button">
+      <span className={cnFlag}>{content.flag}</span>
+      <p>{content.text}</p>
+    </button>
+  );
+};
 
 export default ButtonElement;
+
+ButtonElement.propTypes = {
+  content: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    text: PropTypes.string.isRequired,
+    flag: PropTypes.string.isRequired,
+  }).isRequired,
+  checked: PropTypes.bool,
+  changeMode: PropTypes.func.isRequired,
+};
+
+ButtonElement.defaultProps = {
+  checked: false,
+};
