328ed227c60c6287ed51117ac02197528c0b7c36
Elmira Dzhy <elmira.freshcode@gmail.com>
2023-08-17 01:05:16 +0300

    [Refactor] Refactor NextButton - apply eslint and propTypes

--- client/src/components/NextButton/NextButton.js
+++ client/src/components/NextButton/NextButton.js
@@ -1,14 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './NextButton.module.sass';
 
 const NextButton = (props) => {
   const { submit } = props;
 
   return (
-    <div onClick={submit} className={styles.buttonContainer}>
+    <div
+      role="button"
+      tabIndex="0"
+      onKeyUp="handleKeyUp(event)"
+      onClick={submit}
+      className={styles.buttonContainer}
+    >
       <span>Next</span>
     </div>
   );
+};
+
+NextButton.propTypes = {
+  submit: PropTypes.func.isRequired,
 };
 
 export default NextButton;
