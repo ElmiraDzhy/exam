d52bbdb69821b00003444003deed16efa2e508e4
Elmira Dzhy <elmira.freshcode@gmail.com>
2023-08-16 11:49:46 +0300

    [Refactor] Refactor OfferForModerator - apply eslint and propTypes

--- client/src/pages/ModeratorOfferPage/OfferForModerator/index.jsx
+++ client/src/pages/ModeratorOfferPage/OfferForModerator/index.jsx
@@ -1,21 +1,31 @@
 import React from 'react';
-import styles from './OfferForModerator.module.scss'
+import PropTypes from 'prop-types';
+import styles from './OfferForModerator.module.scss';
 
 const OfferForModerator = (props) => {
-    const {offer, confirm, rescind} = props;
+  const { offer, confirm, rescind } = props;
 
-    return (
-        <article className={styles["container"]}>
-            <div className={styles["container-content"]}>
-                <p className={styles["container-text"]}>{offer.text}</p>
-                {offer.fileName && <img className={styles["container-image"]} src={offer.fileName} alt={'offer file'} />}
-            </div>
-            <div className={styles['button-wrapper']}>
-                <button className={styles["container-button"]} onClick={() => confirm(offer)}>confirm</button>
-                <button className={styles["container-button"]} onClick={() => rescind(offer)}>rescind</button>
-            </div>
-        </article>
-    );
-}
+  return (
+    <article className={styles.container}>
+      <div className={styles['container-content']}>
+        <p className={styles['container-text']}>{offer.text}</p>
+        {offer.fileName && <img className={styles['container-image']} src={offer.fileName} alt="offer file" />}
+      </div>
+      <div className={styles['button-wrapper']}>
+        <button className={styles['container-button']} onClick={() => confirm(offer)} type="button">confirm</button>
+        <button className={styles['container-button']} onClick={() => rescind(offer)} type="button">rescind</button>
+      </div>
+    </article>
+  );
+};
+
+OfferForModerator.propTypes = {
+  offer: PropTypes.shape({
+    text: PropTypes.string,
+    fileName: PropTypes.string,
+  }).isRequired,
+  confirm: PropTypes.func.isRequired,
+  rescind: PropTypes.func.isRequired,
+};
 
 export default OfferForModerator;
