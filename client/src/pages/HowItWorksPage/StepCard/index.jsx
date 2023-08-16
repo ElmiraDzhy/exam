7cd4d9670e7daca52812e80f3b94972c5e1c84c2
Elmira Dzhy <elmira.freshcode@gmail.com>
2023-08-16 15:23:32 +0300

    [Refactor] Refactor StepCard - apply eslint and propTypes

--- client/src/pages/HowItWorksPage/StepCard/index.jsx
+++ client/src/pages/HowItWorksPage/StepCard/index.jsx
@@ -1,24 +1,22 @@
 import React from 'react';
-import styles from './StepCard.module.scss'
 import PropTypes from 'prop-types';
+import styles from './StepCard.module.scss';
 
-const StepCard = ({stepNumber, stepText}) => {
-return(
+const StepCard = ({ stepNumber, stepText }) => {
+  return (
     <article className={styles.container}>
-        <p className={styles['step-number']}>{stepNumber}.</p>
-        <p className={styles['step-text']}>{stepText}</p>
+      <p className={styles['step-number']}>
+        {stepNumber}
+        .
+      </p>
+      <p className={styles['step-text']}>{stepText}</p>
     </article>
-);
-}
-
-StepCard.propTypes = {
-    stepNumber: PropTypes.string.isRequired,
-    stepText: PropTypes.string.isRequired,
+  );
 };
 
-StepCard.defaultProps = {
-    stepNumber: '1',
-    stepText: 'This is default text for StepCard',
+StepCard.propTypes = {
+  stepNumber: PropTypes.string.isRequired,
+  stepText: PropTypes.string.isRequired,
 };
 
 export default StepCard;
