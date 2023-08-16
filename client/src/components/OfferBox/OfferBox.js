1424489cf8ab4504afb3d253eceab8eab4ec29e8
Elmira Dzhy <elmira.freshcode@gmail.com>
2023-08-17 01:05:39 +0300

    [Refactor] Refactor OfferBox - apply eslint and propTypes

--- client/src/components/OfferBox/OfferBox.js
+++ client/src/components/OfferBox/OfferBox.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Rating from 'react-rating';
 import { withRouter } from 'react-router-dom';
@@ -6,7 +7,7 @@ import isEqual from 'lodash/isEqual';
 import classNames from 'classnames';
 import { confirmAlert } from 'react-confirm-alert';
 import {
-  changeMark,
+  changeMark as changeMarkAction,
   clearChangeMarkError,
   goToExpandedDialog,
   changeShowImage,
@@ -18,30 +19,34 @@ import './confirmStyle.css';
 
 const OfferBox = (props) => {
   const findConversationInfo = () => {
-    const { messagesPreview, id } = props;
-    const participants = [id, props.data.User.id];
+    const { messagesPreview, id, data: { User } } = props;
+    const participants = [id, User.id];
     participants.sort((participant1, participant2) => participant1 - participant2);
-    for (let i = 0; i < messagesPreview.length; i++) {
-      if (isEqual(participants, messagesPreview[i].participants)) {
-        return {
-          participants: messagesPreview[i].participants,
-          id: messagesPreview[i].id,
-          blackList: messagesPreview[i].blackList,
-          favoriteList: messagesPreview[i].favoriteList,
+    let foundMessage = null;
+
+    messagesPreview.forEach((messagePreview) => {
+      if (isEqual(participants, messagePreview.participants)) {
+        foundMessage = {
+          participants: messagePreview.participants,
+          id: messagePreview.id,
+          blackList: messagePreview.blackList,
+          favoriteList: messagePreview.favoriteList,
         };
       }
-    }
-    return null;
+    });
+
+    return foundMessage;
   };
 
   const resolveOffer = () => {
+    const { setOfferStatus, data } = props;
     confirmAlert({
       title: 'confirm',
       message: 'Are u sure?',
       buttons: [
         {
           label: 'Yes',
-          onClick: () => props.setOfferStatus(props.data.User.id, props.data.id, 'resolve'),
+          onClick: () => setOfferStatus(data.User.id, data.id, 'resolve'),
         },
         {
           label: 'No',
@@ -66,9 +71,10 @@ const OfferBox = (props) => {
     });
   };
 
-  const changeMark = (value) => {
-    props.clearError();
-    props.changeMark({
+  const changeMarkHandler = (value) => {
+    const { clearError, changeMark } = props;
+    clearError();
+    changeMark({
       mark: value,
       offerId: props.data.id,
       isFirst: !props.data.mark,
@@ -77,7 +83,7 @@ const OfferBox = (props) => {
   };
 
   const offerStatus = () => {
-    const { status } = props.data;
+    const { data: { status } } = props;
     if (status === CONSTANTS.OFFER_STATUS_REJECTED) {
       return <i className={classNames('fas fa-times-circle reject', styles.reject)} />;
     } if (status === CONSTANTS.OFFER_STATUS_WON) {
@@ -87,15 +93,22 @@ const OfferBox = (props) => {
   };
 
   const goChat = () => {
-    props.goToExpandedDialog({ interlocutor: props.data.User, conversationData: findConversationInfo() });
+    props.goToExpandedDialog({
+      interlocutor: props.data.User,
+      conversationData: findConversationInfo(),
+    });
   };
 
   const {
-    data, role, id, contestType,
+    data, role, id, contestType, needButtons,
   } = props;
   const {
-    avatar, firstName, lastName, email, rating,
-  } = props.data.User;
+    data: {
+      User: {
+        avatar, firstName, lastName, email, rating,
+      },
+    },
+  } = props;
   return (
     <div className={styles.offerContainer}>
       {offerStatus()}
@@ -123,41 +136,64 @@ const OfferBox = (props) => {
                   src={`${CONSTANTS.STATIC_IMAGES_PATH}star-outline.png`}
                   alt="star-outline"
                 />
-)}
+              )}
               readonly
             />
           </div>
         </div>
         <div className={styles.responseConainer}>
           {
-                        contestType === CONSTANTS.LOGO_CONTEST
-                          ? (
-                            <img
-                              onClick={() => props.changeShowImage({ imagePath: data.fileName, isShowOnFull: true })}
-                              className={styles.responseLogo}
-                              src={`${data.fileName}`}
-                              alt="logo"
-                            />
-                          )
-                          : <span className={styles.response}>{data.text}</span>
-                    }
+            contestType === CONSTANTS.LOGO_CONTEST
+              ? (
+                <img
+                  onClick={() => props.changeShowImage(
+                    { imagePath: data.fileName, isShowOnFull: true },
+                  )}
+                  className={styles.responseLogo}
+                  src={`${data.fileName}`}
+                  alt="logo"
+                />
+              )
+              : <span className={styles.response}>{data.text}</span>
+          }
           {data.User.id !== id && (
           <Rating
             fractions={2}
             fullSymbol={<img src={`${CONSTANTS.STATIC_IMAGES_PATH}star.png`} alt="star" />}
             placeholderSymbol={<img src={`${CONSTANTS.STATIC_IMAGES_PATH}star.png`} alt="star" />}
             emptySymbol={<img src={`${CONSTANTS.STATIC_IMAGES_PATH}star-outline.png`} alt="star" />}
-            onClick={changeMark}
+            onClick={changeMarkHandler}
             placeholderRating={data.mark}
           />
           )}
         </div>
-        {role !== CONSTANTS.CREATOR && <i onClick={goChat} className="fas fa-comments" />}
+        {role !== CONSTANTS.CREATOR && (
+        <i
+          onClick={goChat}
+          className="fas fa-comments"
+        />
+        )}
       </div>
-      {props.needButtons(data.status) && (
+      {needButtons(data.status) && (
       <div className={styles.btnsContainer}>
-        <div onClick={resolveOffer} className={styles.resolveBtn}>Resolve</div>
-        <div onClick={rejectOffer} className={styles.rejectBtn}>Reject</div>
+        <div
+          role="button"
+          tabIndex="0"
+          onKeyUp="handleKeyUp(event)"
+          onClick={resolveOffer}
+          className={styles.resolveBtn}
+        >
+          Resolve
+        </div>
+        <div
+          role="button"
+          tabIndex="0"
+          onKeyUp="handleKeyUp(event)"
+          onClick={rejectOffer}
+          className={styles.rejectBtn}
+        >
+          Reject
+        </div>
       </div>
       )}
     </div>
@@ -165,7 +201,7 @@ const OfferBox = (props) => {
 };
 
 const mapDispatchToProps = (dispatch) => ({
-  changeMark: (data) => dispatch(changeMark(data)),
+  changeMark: (data) => dispatch(changeMarkAction(data)),
   clearError: () => dispatch(clearChangeMarkError()),
   goToExpandedDialog: (data) => dispatch(goToExpandedDialog(data)),
   changeShowImage: (data) => dispatch(changeShowImage(data)),
@@ -178,6 +214,36 @@ const mapStateToProps = (state) => {
   return {
     changeMarkError, id, role, messagesPreview, isShowModal,
   };
+};
+
+OfferBox.propTypes = {
+  setOfferStatus: PropTypes.func.isRequired,
+  clearError: PropTypes.func.isRequired,
+  changeMark: PropTypes.func.isRequired,
+  goToExpandedDialog: PropTypes.func.isRequired,
+  changeShowImage: PropTypes.func.isRequired,
+  needButtons: PropTypes.func.isRequired,
+
+  messagesPreview: PropTypes.arrayOf(PropTypes.object).isRequired,
+  id: PropTypes.number.isRequired,
+  role: PropTypes.string.isRequired,
+  contestType: PropTypes.string.isRequired,
+
+  data: PropTypes.shape({
+    id: PropTypes.number,
+    fileName: PropTypes.string,
+    text: PropTypes.string,
+    status: PropTypes.bool.isRequired,
+    mark: PropTypes.number,
+    User: PropTypes.shape({
+      id: PropTypes.number,
+      avatar: PropTypes.string,
+      firstName: PropTypes.string,
+      lastName: PropTypes.string,
+      email: PropTypes.string,
+      rating: PropTypes.number,
+    }),
+  }).isRequired,
 };
 
 export default withRouter(connect(mapStateToProps, mapDispatchToProps)(OfferBox));
