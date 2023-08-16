01d1ebebdd994cc94495ec34ca42a529692de166
Elmira Dzhy <elmira.freshcode@gmail.com>
2023-08-17 01:08:41 +0300

    [Refactor] Refactor TaglineContestSpecialInfo - apply eslint and propTypes

--- client/src/components/Contest/ContestInfo/TaglineContestSpecialInfo.js
+++ client/src/components/Contest/ContestInfo/TaglineContestSpecialInfo.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './ContestInfo.module.sass';
 
 const TaglineContestSpecialInfo = (props) => {
@@ -7,16 +8,20 @@ const TaglineContestSpecialInfo = (props) => {
     <>
       {nameVenture && (
       <div className={styles.dataContainer}>
-        <span className={styles.label}>Name ventrure</span>
+        <span className={styles.label}>Name venture</span>
         <span className={styles.data}>{nameVenture}</span>
       </div>
       )}
       <div className={styles.dataContainer}>
-        <span className={styles.label}>Type of Taglinee</span>
+        <span className={styles.label}>Type of Tagline</span>
         <span className={styles.data}>{typeOfTagline}</span>
       </div>
     </>
   );
 };
 
+TaglineContestSpecialInfo.propTypes = {
+  nameVenture: PropTypes.string.isRequired,
+  typeOfTagline: PropTypes.string.isRequired,
+};
 export default TaglineContestSpecialInfo;
