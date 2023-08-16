aafee080874cd0ebef1f456eb59e8e65182b0e8f
Elmira Dzhy <elmira.freshcode@gmail.com>
2023-08-17 01:08:51 +0300

    [Refactor] Refactor TryAgain component - apply eslint and propTypes

--- client/src/components/TryAgain/TryAgain.js
+++ client/src/components/TryAgain/TryAgain.js
@@ -1,14 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './TryAgain.module.sass';
 
 const TryAgain = (props) => {
   const { getData } = props;
   return (
     <div className={styles.container}>
-      <span onClick={() => getData()}>Server Error. Try again</span>
+      <span
+        role="button"
+        tabIndex="0"
+        onKeyUp="handleKeyUp(event)"
+        onClick={() => getData()}
+      >
+        Server Error. Try again
+      </span>
       <i className="fas fa-redo" onClick={() => getData()} />
     </div>
   );
+};
+
+TryAgain.propTypes = {
+  getData: PropTypes.func.isRequired,
 };
 
 export default TryAgain;
