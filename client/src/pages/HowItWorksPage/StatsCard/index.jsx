d295282be1e8db190dc294427acc46f7c5f37894
Elmira Dzhy <elmira.freshcode@gmail.com>
2023-08-16 15:25:02 +0300

    [Refactor] Refactor StatsCard - apply eslint and propTypes

--- client/src/pages/HowItWorksPage/StatsCard/index.jsx
+++ client/src/pages/HowItWorksPage/StatsCard/index.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './StatsCard.module.scss';
 
 const StatsCard = (props) => {
-    const {iconSrc} = props;
-    const boldStyles = styles['bold-text'];
-    const textStyles = styles['text'];
-    return (
-        <article className={styles.container}>
-            <img className={styles['icon-stats']} src={iconSrc} alt={"icon"}/>
-            <p>{props.children({boldStyles, textStyles})}</p>
-        </article>
-    )
-}
+  const { iconSrc, children } = props;
+  const boldStyles = styles['bold-text'];
+  const textStyles = styles.text;
+  return (
+    <article className={styles.container}>
+      <img className={styles['icon-stats']} src={iconSrc} alt="icon" />
+      <p>{children({ boldStyles, textStyles })}</p>
+    </article>
+  );
+};
+
+StatsCard.propTypes = {
+  iconSrc: PropTypes.string.isRequired,
+  children: PropTypes.func.isRequired,
+};
 
 export default StatsCard;
