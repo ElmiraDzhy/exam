21332692884c04a21762cb913abfbdc572e4d950
Elmira Dzhy <elmira.freshcode@gmail.com>
2023-08-16 15:26:11 +0300

    [Refactor] Refactor PricingCard - apply eslint and propTypes

--- client/src/pages/HowItWorksPage/PricingCard/index.jsx
+++ client/src/pages/HowItWorksPage/PricingCard/index.jsx
@@ -3,20 +3,20 @@ import PropTypes from 'prop-types';
 import styles from './PricingCard.module.scss';
 
 const PricingCard = (props) => {
-    const headerStyles = styles.header;
-    const textStyles = styles.text;
-    const arrowStyles = styles['arrow-container'];
+  const { children } = props;
+  const headerStyles = styles.header;
+  const textStyles = styles.text;
+  const arrowStyles = styles['arrow-container'];
 
-    return (
-        <article className={styles.container}>
-            {props.children({headerStyles, textStyles, arrowStyles})}
-        </article>
-    )
-
-}
+  return (
+    <article className={styles.container}>
+      {children({ headerStyles, textStyles, arrowStyles })}
+    </article>
+  );
+};
 
 PricingCard.propTypes = {
-    children: PropTypes.func.isRequired,
+  children: PropTypes.func.isRequired,
 };
 
 export default PricingCard;
