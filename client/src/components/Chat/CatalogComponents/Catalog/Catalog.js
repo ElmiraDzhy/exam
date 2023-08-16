99820751fbcedcd5a4ba1e1b6d72e9c3f0e5d575
Elmira Dzhy <elmira.freshcode@gmail.com>
2023-08-17 00:59:50 +0300

    [Refactor] Refactor Catalog - apply eslint and propTypes

--- client/src/components/Chat/CatalogComponents/Catalog/Catalog.js
+++ client/src/components/Chat/CatalogComponents/Catalog/Catalog.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './Catalog.module.sass';
 
 const Catalog = (props) => {
   const { deleteCatalog, goToCatalog } = props;
-  const { catalogName, chats, id } = props.catalog;
+  const { catalog: { catalogName, chats, id } } = props;
   return (
-    <div className={styles.catalogContainer} onClick={(event) => goToCatalog(event, props.catalog)}>
+    <div
+      role="button"
+      tabIndex="0"
+      onKeyUp="handleKeyUp(event)"
+      className={styles.catalogContainer}
+      onClick={(event) => goToCatalog(event, props.catalog)}
+    >
       <span className={styles.catalogName}>{catalogName}</span>
       <div className={styles.infoContainer}>
         <span>Chats number:  </span>
@@ -16,4 +23,13 @@ const Catalog = (props) => {
   );
 };
 
+Catalog.propTypes = {
+  deleteCatalog: PropTypes.func.isRequired,
+  goToCatalog: PropTypes.func.isRequired,
+  catalog: PropTypes.shape({
+    catalogName: PropTypes.string,
+    chats: PropTypes.arrayOf(PropTypes.object),
+    id: PropTypes.number,
+  }).isRequired,
+};
 export default Catalog;
