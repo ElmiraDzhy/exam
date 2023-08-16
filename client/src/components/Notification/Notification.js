cbac7af0b9166141969931e160111d652158f98a
Elmira Dzhy <elmira.freshcode@gmail.com>
2023-08-17 01:05:26 +0300

    [Refactor] Refactor Notification - apply eslint and propTypes

--- client/src/components/Notification/Notification.js
+++ client/src/components/Notification/Notification.js
@@ -1,15 +1,37 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
 import styles from './Notification.module.sass';
 
-const Notification = (props) => (
-  <div>
-    <br />
-    <span>{props.message}</span>
-    <br />
-    {props.contestId
-            && <span onClick={() => props.history.push(`/contest/${props.contestId}`)} className={styles.goToContest}>Go to contest</span>}
-  </div>
-);
+const Notification = (props) => {
+  const { message, history, contestId } = props;
+  return (
+    <div>
+      <br />
+      <span>{message}</span>
+      <br />
+      {contestId
+            && (
+            <span
+              role="button"
+              tabIndex="0"
+              onKeyUp="handleKeyUp(event)"
+              onClick={() => history.push(`/contest/${contestId}`)}
+              className={styles.goToContest}
+            >
+              Go to contest
+            </span>
+            )}
+    </div>
+  );
+};
+
+Notification.propTypes = {
+  message: PropTypes.string.isRequired,
+  contestId: PropTypes.number.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
 
 export default withRouter(Notification);
