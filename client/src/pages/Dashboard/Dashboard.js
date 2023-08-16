cc6604343550cde62a99d2a37f07da07f79be3ab
Elmira Dzhy <elmira.freshcode@gmail.com>
2023-08-17 01:03:24 +0300

    [Refactor] Refactor Dashboard - apply eslint and propTypes

--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import CONSTANTS from '../../constants';
 import CustomerDashboard from '../../components/CustomerDashboard/CustomerDashboard';
@@ -6,7 +7,7 @@ import CreatorDashboard from '../../components/CreatorDashboard/CreatorDashboard
 import Header from '../../components/Header/Header';
 
 const Dashboard = (props) => {
-  const { role, history } = props;
+  const { role, history, match } = props;
 
   useEffect(() => {
     if (role === 'moderator') {
@@ -19,13 +20,21 @@ const Dashboard = (props) => {
       <Header />
       {
                 role === CONSTANTS.CUSTOMER
-                  ? <CustomerDashboard history={history} match={props.match} />
-                  : <CreatorDashboard history={history} match={props.match} />
+                  ? <CustomerDashboard history={history} match={match} />
+                  : <CreatorDashboard history={history} match={match} />
             }
     </div>
   );
 };
 
 const mapStateToProps = (state) => state.userStore.data;
+
+Dashboard.propTypes = {
+  role: PropTypes.string.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+  match: PropTypes.shape({}).isRequired,
+};
 
 export default connect(mapStateToProps)(Dashboard);
