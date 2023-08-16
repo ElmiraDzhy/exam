f92e701a22277817e83665d6a30888aa04110f71
Elmira Dzhy <elmira.freshcode@gmail.com>
2023-08-17 01:05:08 +0300

    [Refactor] Refactor NameContestSpecialInfo - apply eslint and propTypes

--- client/src/components/Contest/ContestInfo/NameContestSpecialInfo.js
+++ client/src/components/Contest/ContestInfo/NameContestSpecialInfo.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './ContestInfo.module.sass';
 
 const NameContestSpecialInfo = (props) => {
@@ -15,6 +16,11 @@ const NameContestSpecialInfo = (props) => {
       </div>
     </>
   );
+};
+
+NameContestSpecialInfo.propTypes = {
+  typeOfName: PropTypes.string.isRequired,
+  styleName: PropTypes.string.isRequired,
 };
 
 export default NameContestSpecialInfo;
