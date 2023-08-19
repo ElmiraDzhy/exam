9b61f690a59470a70fdad3d186868403689339ef
Elmira Dzhy <elmira.freshcode@gmail.com>
2023-08-19 17:18:00 +0300

    [Refactor] Refactor LoginForm - rewrite on functional component

--- client/src/components/LoginForm/LoginForm.js
+++ client/src/components/LoginForm/LoginForm.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Form, Formik } from 'formik';
@@ -8,77 +8,76 @@ import FormInput from '../FormInput/FormInput';
 import Schems from '../../validators/validationSchems';
 import Error from '../Error/Error';
 
-class LoginForm extends React.Component {
-  componentWillUnmount() {
-    const { authClearDispatch } = this.props;
-    authClearDispatch();
-  }
+const LoginForm = (props) => {
+  const { authClearDispatch, loginRequestDispatch, history } = props;
+  const { auth: { error, isFetching } } = props;
+  const { submitting } = props;
 
-    clicked = (values) => {
-      const { loginRequestDispatch, history } = this.props;
-      loginRequestDispatch({ data: values, history });
+  useEffect(() => {
+    return () => {
+      authClearDispatch();
     };
+  }, []);
 
-    render() {
-      const { auth: { error, isFetching } } = this.props;
-      const { submitting, authClearDispatch } = this.props;
+  const clicked = (values) => {
+    loginRequestDispatch({ data: values, history });
+  };
 
-      const formInputClasses = {
-        container: styles.inputContainer,
-        input: styles.input,
-        warning: styles.fieldWarning,
-        notValid: styles.notValid,
-        valid: styles.valid,
-      };
+  const formInputClasses = {
+    container: styles.inputContainer,
+    input: styles.input,
+    warning: styles.fieldWarning,
+    notValid: styles.notValid,
+    valid: styles.valid,
+  };
 
-      return (
-        <div className={styles.loginForm}>
-          {error && (
-          <Error
-            data={error.data}
-            status={error.status}
-            clearError={authClearDispatch}
+  return (
+    <div className={styles.loginForm}>
+      {error && (
+      <Error
+        data={error.data}
+        status={error.status}
+        clearError={authClearDispatch}
+      />
+      )}
+      <h2>LOGIN TO YOUR ACCOUNT</h2>
+      <Formik
+        initialValues={{
+          email: '',
+          password: '',
+        }}
+        onSubmit={clicked}
+        validationSchema={Schems.LoginSchem}
+      >
+        <Form>
+          <FormInput
+            classes={formInputClasses}
+            name="email"
+            type="text"
+            label="Email Address"
           />
-          )}
-          <h2>LOGIN TO YOUR ACCOUNT</h2>
-          <Formik
-            initialValues={{
-              email: '',
-              password: '',
-            }}
-            onSubmit={this.clicked}
-            validationSchema={Schems.LoginSchem}
+          <FormInput
+            classes={formInputClasses}
+            name="password"
+            type="password"
+            label="Password"
+          />
+          <button
+            type="submit"
+            disabled={submitting}
+            className={styles.submitContainer}
           >
-            <Form>
-              <FormInput
-                classes={formInputClasses}
-                name="email"
-                type="text"
-                label="Email Address"
-              />
-              <FormInput
-                classes={formInputClasses}
-                name="password"
-                type="password"
-                label="Password"
-              />
-              <button
-                type="submit"
-                disabled={submitting}
-                className={styles.submitContainer}
-              >
-                <span className={styles.inscription}>
-                  {isFetching
-                    ? 'Submitting...'
-                    : 'LOGIN'}
-                </span>
-              </button>
-            </Form>
-          </Formik>
-        </div>
-      );
-    }
-}
+            <span className={styles.inscription}>
+              {isFetching
+                ? 'Submitting...'
+                : 'LOGIN'}
+            </span>
+          </button>
+        </Form>
+      </Formik>
+    </div>
+  );
+};
 
 const mapStateToProps = (state) => {
   const { auth } = state;
