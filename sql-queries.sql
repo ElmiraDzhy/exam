b54faa3d5c0d49b196fa3c75767ef88cac56d627
Elmira Dzhy <elmira.freshcode@gmail.com>
2023-08-30 14:01:19 +0300

    [Bug Fix] Fix sql query #10 task

--- sql-queries.sql
+++ sql-queries.sql
@@ -4,12 +4,12 @@ GROUP BY role;
 
 -- TASK #10
 UPDATE users
-SET balance = balance + prizeSum FROM (SELECT users.id, users.first_name, sum(c.prize) * 0.1 as prizeSum FROM users
+SET balance = balance + prizeSum FROM (SELECT users.id as customer_id, users.first_name, sum(c.prize) * 0.1 as prizeSum FROM users
                              JOIN contests AS c ON users.id = c.user_id
-                             WHERE role='customer' AND extract('month' from c.created_at) BETWEEN 7 and 8
-                                 AND( extract('day' from c.created_at) > 25 or extract('day' from c.created_at) < 14)
+                             WHERE users.role='customer' AND (extract('month' from c.created_at) = 12 AND extract('day' from c.created_at) > 25)
+                                OR (extract('month' from c.created_at) = 1 AND extract('day' from c.created_at) < 14)
                              GROUP BY users.id, users.first_name) as "newtable"
-WHERE role = 'customer';
+WHERE users.id = customer_id;
 
 -- TASK #11
 
